498b5f4218375c877a1f4ad93e2989a8019329b9
Yerko47 <yerko.jelcic@usach.cl>
2026-08-05 18:24:02 -0400

    Add utility functions for path management and directory creation

---
+++ src/utils/paths.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+def path_file() -> dict:
+    """
+    Diccionario con las carpetas necesarias para el funcionamiento del código.
+    Si es que la carpeta no existe, se crea mediante este código
+    """
+
+    base = Path(Path(__file__).resolve().parent.parent.parent)
+
+    project_paths = {
+        "raw_file": ("data", "raw"),
+        "processed_file": ("data", "processed"),
+        "events_file": ("data", "events"),
+
+        "model_result_file": ("results", "model"),
+        "prediction_result_file": ("results", "prediction"),
+        "metric_result_file": ("results", "metric"),
+
+        "checkpoint_file": ("results", "checkpoints"),
+
+        "figure_benchmark_file": ("figures", "interpolation", "benchmark"),
+        "figure_history_diff_file": ("figures", "interpolation", "history_diff")
+    }
+
+    paths = {key: base.joinpath(*parts) for key, parts in project_paths.items()}
+    paths["config_file"] = base / "config"
+    paths["config_model_file"] = base / "config" / "models"
+    paths["log_file"] = base / "log"
+
+    for path in paths.values():
+        path.mkdir(parents = True, exist_ok = True)
+
+    paths["base_file"] = base
+
+    return paths
+
+
+def create_new_file(paths: Path, name_file: str, new_name: str) -> Path:
+    """
+    Crear una nueva carpeta para guardar elementos puntuales que no son necesario tener en un diccionario
+    """
+    new_file = paths[name_file] / new_name
+    
+    new_file.mkdir(parents = True, exist_ok = True)
+
+    return new_file
